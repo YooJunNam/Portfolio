77aa7f07a9c8d928813604a2b8492edde3469205
YooJunNam <yjn7448@gmail.com>
2021-09-06 21:09:57 +0900

    project 파일에 새로운 프로젝트 3가지 깃 주소까지 추가.

--- src/Page/Projects.js
+++ src/Page/Projects.js
@@ -5,6 +5,9 @@ import github from "../Img/github.png";
 import "../CSS/Skills.css";
 import portfolio from "../Pdf/portfolio.pdf";
 import portfolioImg from "../Img/portfolio.PNG";
+import blog from "../Img/blog.PNG";
+import shoppingmall from "../Img/shoppingmall.PNG";
+import recollection from "../Img/recollection.PNG";
 
 function Projects() {
   return (
@@ -116,6 +119,109 @@ function Projects() {
 
         <div className="projectGitLink rightProjectGit">
           <a style={{}} href="https://github.com/YooJunNam">
+            <img
+              className="hoverImg"
+              src={github}
+              style={{ height: "40px", width: "40px" }}
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="projectsContainer">
+        <div className="projectBox">
+          <div style={{ margin: "30px" }}>
+            <img width="300px" height="200px" src={blog}></img>
+          </div>
+          <div style={{ margin: "30px" }}>
+            <h3>Mini-Blog</h3>
+            <p>개인 프로젝트</p>
+            <p>사용 언어: React</p>
+            <h4>Simple Description</h4>
+            <p>기본적인 Layout, Html Css를 활용</p>
+            <p>기본적인 Hook 사용 (Usestate)</p>
+          </div>
+        </div>
+        <div className="MobileProjectGitLink">
+          <a href="https://github.com/YooJunNam/Blog">
+            <img
+              className="hoverImg"
+              src={github}
+              style={{ height: "40px", width: "40px" }}
+            />
+          </a>
+        </div>
+        <div className="projectBox">
+          <div style={{ margin: "30px" }}>
+            <img width="300px" height="200px" src={shoppingmall}></img>
+          </div>
+          <div style={{ margin: "30px" }}>
+            <h3>Shoes 쇼핑몰 </h3>
+            <p>협업 프로젝트</p>
+            <p>사용 언어 : HTML, CSS , React framwork 활용</p>
+            <h4>Simple Description</h4>
+            <p>여러가지 React Hook , ContextAPI</p>
+            <p>React Router Dom 으로 SPA 구조 구현</p>
+          </div>
+        </div>
+        <div className="MobileProjectGitLink">
+          <a href="https://github.com/YooJunNam/appleCoding-ShoppingMall">
+            <img
+              className="hoverImg"
+              src={github}
+              style={{ height: "40px", width: "40px" }}
+            />
+          </a>
+        </div>
+        <div className="projectBox">
+          <div style={{ margin: "30px" }}>
+            <img width="300px" height="200px" src={recollection}></img>
+          </div>
+          <div style={{ margin: "30px" }}>
+            <h3>Recollection</h3>
+            <p>협업 프로젝트 ( 진행중 )</p>
+            <p>사용 언어 : HTML, CSS, JAVASCRIPT React</p>
+            <h4>Simple Description</h4>
+            <p>외부 카카오 지도 API 활용</p>
+            <p>반응형 모바일 뷰 구축(진행중)</p>
+          </div>
+        </div>
+        <div className="MobileProjectGitLink">
+          <a href="https://github.com/YooJunNam/recollection">
+            <img
+              className="hoverImg"
+              src={github}
+              style={{ height: "40px", width: "40px" }}
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className="projectGitLinkContainer">
+        <div className="projectGitLink">
+          <a href="https://github.com/YooJunNam/Blog">
+            <img
+              className="hoverImg"
+              src={github}
+              style={{ height: "40px", width: "40px" }}
+            />
+          </a>
+        </div>
+        <div className="projectGitLink leftProjectGit">
+          <a
+            style={{}}
+            href="https://github.com/YooJunNam/appleCoding-ShoppingMall"
+          >
+            <img
+              className="hoverImg"
+              src={github}
+              style={{ height: "40px", width: "40px" }}
+            />
+          </a>
+        </div>
+
+        <div className="projectGitLink rightProjectGit">
+          <a style={{}} href="https://github.com/YooJunNam/recollection">
             <img
               className="hoverImg"
               src={github}
